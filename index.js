7c483965eff6e7df62061be593fd6bd8467cf701
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-11-01 19:47:03 +0530

    new contents added in javascript-study-questions

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 //1)how to reverse a number
 
-let num=123456;
+// let num=123456;
+// let rev=0
+
+// while(num>0)
+// {
+//     digit=num%10
+//     rev=rev*10+digit
+//     num=Math.floor(num/10)
+
+// }
+// console.log(rev);
+
+
+
+
+//2)check if the number is plidrome or not
+
+let str="ardra"
+let temp=str;
 let rev=0
 
-while(num>0)
+while(str>0)
 {
-    digit=num%10
+    digit=num%10;
     rev=rev*10+digit
     num=Math.floor(num/10)
-
 }
-console.log(rev);
+if(temp===rev)
+{
+    console.log("palidrome");
+    
+}
+else{
+    console.log("not palidrome");
+    
+}
