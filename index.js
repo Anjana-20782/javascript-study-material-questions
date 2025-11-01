422604553da2430b717801b6850a0b535045a510
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-11-01 19:52:02 +0530

    new contents added in javascript-study-questions

--- index.js
+++ index.js
@@ -17,37 +17,59 @@
 
 //2)check if the number is plidrome or not
 
-let num=121
-let temp=num;
-let rev=0
+// let num=121
+// let temp=num;
+// let rev=0
 
-while(num>0)
-{
-    digit=num%10;
-    rev=rev*10+digit
-    num=Math.floor(num/10)
-}
-if(temp===rev)
-{
-    console.log("palidrome");
+// while(num>0)
+// {
+//     digit=num%10;
+//     rev=rev*10+digit
+//     num=Math.floor(num/10)
+// }
+// if(temp===rev)
+// {
+//     console.log("palidrome");
     
-}
-else{
-    console.log("not palidrome");
+// }
+// else{
+//     console.log("not palidrome");
     
+// }
+
+
+// let str = "malayalam";
+// let reversed = "";
+
+// for (let i = str.length - 1; i >= 0; i--) {
+//   reversed += str[i];
+// }
+
+// if (str === reversed) {
+//   console.log("Palindrome String");
+// } else {
+//   console.log("Not a Palindrome");
+// }
+
+
+
+
+
+//3)check if the number is odd or even
+
+let even=0
+let odd=0
+
+for(i=0;i<=100;i++)
+{
+    if(i%2==0)
+    {
+        even+=i
+    }
+    else{
+        odd+=i
+    }
 }
-
-
-let str = "malayalam";
-let reversed = "";
-
-for (let i = str.length - 1; i >= 0; i--) {
-  reversed += str[i];
-}
-
-if (str === reversed) {
-  console.log("Palindrome String");
-} else {
-  console.log("Not a Palindrome");
-}
+console.log("odd:"+odd);
+console.log("even:"+even);
 
